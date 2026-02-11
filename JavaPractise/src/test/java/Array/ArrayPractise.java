534397075e104588b83174a1819a2c410b14d8a7
Jass <jaswindersingh050@gmail.com>
2026-02-12 01:38:48 +0530

    Added static data

--- JavaPractise/src/test/java/Array/ArrayPractise.java
+++ JavaPractise/src/test/java/Array/ArrayPractise.java
@@ -3,7 +3,7 @@ package Array;
 public class ArrayPractise {
     public static void main(String[] args) {
 
-        String ajay[]={"vicky ","ricky","picky"};
+        String ajay[]={"vicky ","ricky","pickkky"};
         System.out.println(ajay[2]);
     }
 }
